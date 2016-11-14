ab2c59977cc5613e7f93e9e419dc9818f325003f
siki1793 <saikrishna1793.sk@gmail.com>
2016-11-14 21:28:08 +0530

    database update

--- databaseConnection.js
+++ databaseConnection.js
@@ -5,7 +5,7 @@
 var url = "mongodb://sai:sai@ds151927.mlab.com:51927/mobiledatausage"
 exports.db1 = function(){
 	//first connection to mobiledata database 
-	var dburl1 = url + "mobiledata" ;
+	var dburl1 = url ;
 
 	return require('mongojs').connect(dburl1);
 	
