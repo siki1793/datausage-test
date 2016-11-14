f2855084d9769eb588d29371df0fcb734acdc077
siki1793 <saikrishna1793.sk@gmail.com>
2016-11-14 16:56:14 +0530

    database connection mlabs url change

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,7 +1,8 @@
 //var url = 'mongodb://mongodb-1.recharge-plan-reco.4447.mongodbdns.com:27000,mongodb-0.recharge-plan-reco.4447.mongodbdns.com:27000,mongodb-2.recharge-plan-reco.4447.mongodbdns.com:27000/'
 
 
-var url = "localhost:27017/"
+// var url = "localhost:27017/"
+var url = "mongodb://sai:sai@ds151927.mlab.com:51927/mobiledatausage"
 exports.db1 = function(){
 	//first connection to mobiledata database 
 	var dburl1 = url + "mobiledata" ;
