4b995008011ed2a96d9701dc7b6fee321f7c331a
siki1793 <saikrishna1793.sk@gmail.com>
2016-11-15 19:29:02 +0530

    updated new sync

--- routes/newsync.js
+++ routes/newsync.js
@@ -23,7 +23,7 @@ newsync.post(function(req,res){
 
 	// decrypting the message received	
 	try {
-		console.log("req handler 'newsync' was called.");
+		console.log("req handler 'test' was called.");
 
 		//console.log(req.body);
 		//console.log("data received");
@@ -38,14 +38,85 @@ newsync.post(function(req,res){
 	
 
 
-		var coll = "newdatainfo";
-		var getlogs = db1.collection(coll);
+		var coll1 = "totaldatacol";
+		var coll2="appwisecol";
+
+		var totaldatacol = db1.collection(coll1);
+		var appwisecol=db1.collection(coll2);
+		//var updater=db1.collection("updater");
+		//var splitedUserData=db1.collection("splitedUserData");
 		/*getlogs.find({},function(err,docs){
 			if(err){console.log(err);}
 			console.log("database response")					
 		})
 		 */
-		getlogs.find({"imei" : received.imei},function(err,docs){
+		/*if(received.reset){
+			updater.delete({},function(err){
+				if(err){console.log(err);}
+				else{
+					for ( var i = 0; i< received.data.length;i++) {//updating data with sunc time and ip address
+						serverDate = new Date();
+						//offset = date.getTimezoneOffset() * 60000;
+						//date = new Date(date.valueOf() - offset);
+						console.log(received.data[i].date);
+						received.data[i].date = new Date(received.data[i].date);
+						console.log(received.data[i].date);
+						console.log(received.data[i].date.toISOString());
+						received.data[i].server_synctime = serverDate;
+						//received[i].server_synctime = date.toISOString();
+						received.data[i].ip = req.connection.remoteAddress;
+					}//closing for loop
+					updater.insert(received,function(err,doc){
+							if(err){console.log(err);}
+							else{
+								splitedUserData.aggregate([{$match : {"imei" : received.imei}},{$project : { "_id":0, "imei" : 1 } }],function(err,docid){
+									if(err){colsole.log(err);}
+									else{
+										if(docid.length==0){
+											splitedUserData.insert(recieved,function(err,doc){
+												if(err){console.log(err);
+														
+												}
+												else{
+													console.log("new user data inserted!");
+													res.write("new user data inserted!");
+														res.end();
+												}
+											});
+										
+										}//closing if docid.length
+										else{
+											splitedUserData.update({"imei":received.imei},{$push : { data:{$each: received.data }}},function(err,docv){
+												if(err){console.log(err);}
+												else{
+													console.log("already existing users data updated!");
+													res.write("existing users data updated!");
+													res.end();
+
+												}//else closing
+											});//update query closing
+										}//else closing
+													}//closing else bada wala
+								});
+
+							}
+					});//updater insert closing
+
+				}//else closing
+			});//after function closing)
+			}//updater.delete closing
+			else{
+
+			}//closing else if restart is false
+		//}
+
+	*/
+
+
+
+
+
+		totaldatacol.find({"imei" : received.imei},function(err,docs){
 			if(err){
 				console.log(err);
 			}
@@ -57,66 +128,103 @@ newsync.post(function(req,res){
 					// if length of docs is 0 it means user is new
 					// add the document as it is by changing the date to Dateobject and add server time
 					console.log("new user requesting");
-
-					for ( var i = 0; i< received.data.length;i++) {
 						serverDate = new Date();
+						console.log(received.totalData[0].date);
+						received.totalData[0].date = new Date(received.totalData[0].date);
+						console.log(received.totalData[0].date);
+						console.log(received.totalData[0].date.toISOString());
+						received.totalData[0].server_synctime = serverDate;
+					
+					for ( var i = 0; i< received.applicationData.length;i++) {
 						//offset = date.getTimezoneOffset() * 60000;
 						//date = new Date(date.valueOf() - offset);
-						console.log(received.data[i].date);
-						received.data[i].date = new Date(received.data[i].date);
-						console.log(received.data[i].date);
-						console.log(received.data[i].date.toISOString());
-						received.data[i].server_synctime = serverDate;
+						console.log(received.applicationData[i].date);
+						received.applicationData[i].date = new Date(received.applicationData[i].date);
+						console.log(received.applicationData[i].date);
+						console.log(received.applicationData[i].date.toISOString());
+						received.applicationData[i].server_synctime = serverDate;
 						//received[i].server_synctime = date.toISOString();
-						received.data[i].ip = req.connection.remoteAddress;
+						received.applicationData[i].ip = req.connection.remoteAddress;
 					}
 
-					getlogs.insert(received,function(err,doc){
+					totaldatacol.insert({"mobileNumber":received.mobileNumber,"imei":received.imei,"totalData":received.totalData},function(err,doc){
 						if(err){
 							console.log(err);
 							res.write(0);
 							res.end();
 						}
 						else{
+								console.log("successfully entered into totaldatacol!!");
+										appwisecol.insert({"mobileNumber":received.mobileNumber,"imei":received.imei,"applicationData":received.applicationData},function(err,doc){
+											if(err){
+												console.log(err);
+												res.write(0);
+												res.end();
 
-										console.log("hello2");
-										console.log("successfully synced data " + req.connection.remoteAddress);
-										//res.end();   // close connection
-										res.write("inserted into database"); 
-										res.end();
-						}	 //end else
+											}
+											else{
+												console.log("successfully entered into appwisecol");
+												console.log("successfully synced data " + req.connection.remoteAddress);
+												res.write("inserted into database"); 
+												res.end();
+											}
+										
+										}); 		
+
+						}				
 					}); // end insert query
 				}// end if condition
 				else{
 					// get current id 
 					console.log("in else");
-					getlogs.aggregate([{$match : {"imei" : received.imei}},{$project : { "_id":0, "imei" : 1 } }],function(err,docid){
+					totaldatacol.aggregate([{$match : {"imei" : received.imei}},{$project : { "_id":0 ,"imei":1} }],function(err,docid){
 						if(err){
 							console.log(err);
 						}
 						else{
 								console.log("Updating recieved data")
-								for ( var i = 0; i< received.data.length;i++) {
-									serverDate = new Date();
-									received.data[i].date = new Date(received.data[i].date);
-									received.data[i].server_synctime = serverDate;
-									received.data[i].ip = req.connection.remoteAddress;
-								}
-								console.log(" recieved data updated.....")
+								serverDate = new Date();
+								console.log(received.totalData[0].date);
+								received.totalData[0].date = new Date(received.totalData[0].date);
+								console.log(received.totalData[0].date);
+								console.log(received.totalData[0].date.toISOString());
+								received.totalData[0].server_synctime = serverDate;
+							
+							for ( var i = 0; i< received.applicationData.length;i++) {
+								//offset = date.getTimezoneOffset() * 60000;
+								//date = new Date(date.valueOf() - offset);
+								console.log(received.applicationData[i].date);
+								received.applicationData[i].date = new Date(received.applicationData[i].date);
+								console.log(received.applicationData[i].date);
+								console.log(received.applicationData[i].date.toISOString());
+								received.applicationData[i].server_synctime = serverDate;
+								//received[i].server_synctime = date.toISOString();
+								received.applicationData[i].ip = req.connection.remoteAddress;
+							}
+							console.log(" recieved data updated.....")
 								console.log("Strting updation into database...")
 					
-								getlogs.update({"imei" : received.imei},{$push : { data:{$each: received.data }}},function(err){
+								totaldatacol.update({"imei" : received.imei},{$push : { totalData:{$each: received.totalData }}},function(err){
 									if(err){
 										console.log(err);
 										console.log("error in writing to database");
 										res.end();
 									}
-									else{console.log("successfully updated into db...no error detected");
-												console.log("hello3");
+									else{
+										console.log("successfully updated into totaldatacol!!");
+										appwisecol.update({"imei":received.imei},{$push:{applicationData:{$each:received.applicationData}}},function(err){
+											if(err){console.log(err);}
+											else{
+												console.log("successfully updated into appwisecol!!");
 												console.log("successfully synced data " + req.connection.remoteAddress);
 												console.log("Data got updated!!");
 												res.write("Successfully inserted into database");
 												res.end();   // close connection
+
+											}
+										});
+
+												
 
 										//	}
 
