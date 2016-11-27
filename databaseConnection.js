6f988b66c4df1c3846b86dfaf068a6fadda86fa4
siki1793 <saikrishna1793.sk@gmail.com>
2016-11-28 02:15:29 +0530

    cleanning

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,7 +1,3 @@
-//var url = 'mongodb://mongodb-1.recharge-plan-reco.4447.mongodbdns.com:27000,mongodb-0.recharge-plan-reco.4447.mongodbdns.com:27000,mongodb-2.recharge-plan-reco.4447.mongodbdns.com:27000/'
-
-
-// var url = "localhost:27017/"
 var url = "mongodb://sai:sai@ds151927.mlab.com:51927/mobiledatausage"
 exports.db1 = function(){
 	//first connection to mobiledata database 
@@ -10,11 +6,3 @@ exports.db1 = function(){
 	return require('mongojs').connect(dburl1);
 	
 }
-
-/*
-//second connection to Circle database here it is Karnataka
-exports.db2 = function(circle){
-	var dburl2 = url + circle ;
-	return require('mongojs').connect(dburl2);
-}
-*/
